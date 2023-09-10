4ea8992bba0586affa229e1f40fe5cb83b4572f3
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2023-09-10 13:03:29 +0530

    Create challenge3.sol

---
+++ challenge3.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AdminOnly {
+    address public admin;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function.");
+        _;
+    }
+
+    function changeAdmin(address newAdmin) public onlyAdmin {
+        admin = newAdmin;
+    }
+    
+}
